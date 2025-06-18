60b41ca9a19dce221c6e32a6701287a253d07c8b
Satyam Kumar <satyam.kumar@ayuvya.com>
2025-06-18 16:14:25 +0530

    changes in deepLink to enable open app at specific path/page after clicking on deep link

--- public/deeplink.js
+++ public/deeplink.js
@@ -1,10 +1,28 @@
-const fallbackUrl = "https://play.google.com/store/apps/details?id=com.kundlitalk";
+function getQueryParam(name) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
 
-const intentUrl =
-  "intent://openapp.html#Intent;" +
-  "scheme=https;" +
-  "package=com.kundlitalk;" +
-  "S.browser_fallback_url=" + encodeURIComponent(fallbackUrl) + ";" +
-  "end";
+const inAppUrl = getQueryParam("url") || "https://appkundli.innovatia.co.in/";
 
-window.location.replace(intentUrl);
+// ✅ Just redirect directly to the URL
+// This lets the Android App Link system trigger the app and pass full URL to React Native
+window.location.href = inAppUrl;
+
+
+// function getQueryParam(name) {
+//   const urlParams = new URLSearchParams(window.location.search);
+//   return urlParams.get(name);
+// }
+
+// const inAppUrl = getQueryParam("url") || "https://appkundli.innovatia.co.in/";
+// const fallbackUrl = "https://play.google.com/store/apps/details?id=com.kundlitalk";
+
+// const intentUrl =
+//   "intent://openapp.html?url=" + encodeURIComponent(inAppUrl) +
+//   "#Intent;scheme=https;" +
+//   "package=com.kundlitalk;" +
+//   "S.browser_fallback_url=" + encodeURIComponent(fallbackUrl) + ";" +
+//   "end";
+
+// // window.location.replace(intentUrl);
